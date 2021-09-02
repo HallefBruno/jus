c94d8250390d64f743c009b6627596286c20585c
Hallef <hallefbruno@hotmail.com>
2021-09-01 22:56:21 -0300

    Adicionado endereço remoto

--- src/main/java/com/process/jus/JusApplication.java
+++ src/main/java/com/process/jus/JusApplication.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JusApplication {
@@ -21,24 +20,24 @@ public class JusApplication {
         SpringApplication.run(JusApplication.class, args);
     }
     
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-
-            Usuario usuario = new Usuario();
-            usuario.setEmail("usuario@email.com");
-            usuario.setPerfil(PerfilEnum.ROLE_USUARIO);
-            usuario.setSenha(SenhaUtils.gerarBCrypt("123456"));
-            this.usuarioRepository.save(usuario);
-
-            Usuario admin = new Usuario();
-            admin.setEmail("admin@email.com");
-            admin.setPerfil(PerfilEnum.ROLE_ADMIN);
-            admin.setSenha(SenhaUtils.gerarBCrypt("123456"));
-            this.usuarioRepository.save(admin);
-
-        };
-    }
+//    @Bean
+//    public CommandLineRunner commandLineRunner() {
+//        return args -> {
+//
+//            Usuario usuario = new Usuario();
+//            usuario.setEmail("usuario@email.com");
+//            usuario.setPerfil(PerfilEnum.ROLE_USUARIO);
+//            usuario.setSenha(SenhaUtils.gerarBCrypt("123456"));
+//            this.usuarioRepository.save(usuario);
+//
+//            Usuario admin = new Usuario();
+//            admin.setEmail("admin@email.com");
+//            admin.setPerfil(PerfilEnum.ROLE_ADMIN);
+//            admin.setSenha(SenhaUtils.gerarBCrypt("123456"));
+//            this.usuarioRepository.save(admin);
+//
+//        };
+//    }
     
 //    Unirest.setTimeouts(0, 0);
 //        HttpResponse<String> response = Unirest.post("https://kb-authorization-server.herokuapp.com/oauth/token")
